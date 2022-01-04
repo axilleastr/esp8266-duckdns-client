5652376b694e23e4de1bd826a997fac26ddd014e
axilleastr <axilleastriant10@gmail.com>
2022-01-05 01:18:30 +0200

    esp: Initial commit with esp update client
    ESP8266 DuckDNS update client.
    Updates the DuckDNS domain IP at a one minute interval.

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# esp-ddns
-DDNS client update running on esp8266 board
+# esp8266-duckdns-client
+
+[DuckDNS](https://www.duckdns.org/) DDNS update client running on esp8266 board
+
+## How to use it
+
+In order to use this DDNS update client, update:
+
+1. the [wifi credentials](https://github.com/axilleastr/esp-ddns/blob/main/ddns.ino#L7-L8)
+1. the [DucksDNS domain and token](https://github.com/axilleastr/esp-ddns/blob/main/ddns.ino#L17-L18)
+
+
+## Troubleshooting
+
+Check your serial monitor logs.
+
+You can do that either from the Arduino IDE or (if you are a terminal guy) from your terminal, using `screen`:
+
+```bash
+screen -L /dev/ttyUSB0 9600
+```
+
+## TODOs
+
+1. Cache the currentIP, so that we can compare with the previous one, and update the client only in case of mismatching. This way, we reduce the amount of redundant calls and updates
+1. Update the timestamp to work in a desired timezone
+1. Create a webserver interface, so that we can observe updates and changes easily
+
---
+++ ddns.ino
@@ -0,0 +1,113 @@
+#include <ESP8266HTTPClient.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+const char * ssid = "<ssid>";
+const char * password = "<password>";
+
+ESP8266WebServer server(80);
+
+// Define NTP Client to get time
+WiFiUDP udp;
+NTPClient timeClient(udp, "pool.ntp.org");
+
+// Duckdns variables
+String domain="<domain>.duckdns.org";
+String token="<token>";
+
+// returns current timestamp
+String getTime() {
+  timeClient.update();
+  return timeClient.getFormattedTime();
+}
+
+// returns the day of the week in a readable format
+String getDay() {
+  timeClient.update();
+  switch(timeClient.getDay()){
+    case 0:
+      return "Sunday";
+      break;
+    case 1:
+      return "Monday";
+      break;
+    case 2:
+      return "Tuesday";
+      break;
+    case 3:
+      return "Wednesday";
+      break;
+    case 4:
+      return "Thursday";
+      break;
+    case 5:
+      return "Friday";
+      break;
+    case 6:
+      return "Saturday";
+      break;
+    default:
+      return "";
+    }
+}
+
+// logger helper function
+void log(String msg){
+  Serial.println(getDay() + " " + getTime() + " " + msg);
+}
+
+// returns router's public IP
+String getPublicIP(){
+  WiFiClient client;
+  HTTPClient http;
+  http.begin(client, "http://ifconfig.me/ip");
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    if (httpCode == HTTP_CODE_OK) {
+      return http.getString();
+    }
+  } else {
+    http.end();
+    return "";
+  }
+  http.end();
+}
+
+// Duckdns DDNS update
+void update(String domain, String token){
+  String ip = getPublicIP();
+  String update_url = "http://www.duckdns.org/update?domains=" + domain + "&token=" + token + "&ip=" + ip + "";
+
+  WiFiClient client;
+  HTTPClient http;
+  http.begin(client, update_url);
+  int httpCode = http.GET();
+  if (httpCode > 0) {
+    if (httpCode == HTTP_CODE_OK) {
+      log("IP Updated: " + ip);
+    }
+  } else {
+    http.end();
+    log("Error occured when trying to update IP. Status code received: " + httpCode);
+  }
+  http.end();
+}
+
+void setup() {
+  Serial.begin(9600);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  log("ESP8266 connected to WiFi with IP: " + WiFi.localIP());
+  server.begin();
+}
+
+void loop() {
+  update(domain, token);
+  delay(60000); // one minute delay
+}
